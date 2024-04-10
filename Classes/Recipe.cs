554924a3796e108f70313101798a61ca7d86f555
Joshua <st10296167@vcconnect.edu.za>
2024-04-10 21:56:23 +0200

    Added recipe menu method

--- Classes/Recipe.cs
+++ Classes/Recipe.cs
@@ -9,5 +9,33 @@ namespace JoshuaWood_ST10296167_PROG6221_POE.Classes
     public class Recipe
     {
 
+//------------------------------------------------------------------------------------------------------------------------------------------//
+        private static string recipeMenu()
+        {
+            Console.WriteLine("====== RECIPE GENERATOR ======");
+            Console.WriteLine("Please select an option:");
+            Console.WriteLine("1) Enter recipe details");
+            Console.WriteLine("2) Display recipe");
+            Console.WriteLine("3) Scale recipe");
+            Console.WriteLine("4) Reset recipe");
+            Console.WriteLine("5) Clear recipe");
+            Console.WriteLine("6) Quit");
+            Console.WriteLine();
+            Console.WriteLine("Enter choice: ");
+            string choice = Console.ReadLine();
+            while (choice == null )
+            {
+                Console.WriteLine("Please enter a number between 1-5: ");
+                choice = Console.ReadLine();
+            }
+            return choice;
+        }
+//------------------------------------------------------------------------------------------------------------------------------------------//
+        private void inputRecipeDetails()
+        {
+            int numOfIngredients;
+
+            Console.WriteLine();
+        }
     }
 }
