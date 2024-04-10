28745e4b7c6e873f14e224afa306e88e68d40fdd
Joshua <st10296167@vcconnect.edu.za>
2024-04-10 20:57:30 +0200

    Add project files.

---
+++ Classes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoshuaWood_ST10296167_PROG6221_POE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
