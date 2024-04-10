8c50993ca2416c31a79162552bdefdbfb0893761
Joshua <st10296167@vcconnect.edu.za>
2024-04-10 21:30:17 +0200

    Added Classes folder and necessary classes for solution

---
+++ Classes/Ingredients.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoshuaWood_ST10296167_PROG6221_POE.Classes
+{
+    public class Ingredients
+    {
+        public string ingredientName {  get; set; }
+        public double ingredientQuantity { get; set; }
+        public string measurementUnit { get; set; }
+
+
+    }
+}
---
+++ Classes/Recipe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoshuaWood_ST10296167_PROG6221_POE.Classes
+{
+    public class Recipe
+    {
+
+    }
+}
